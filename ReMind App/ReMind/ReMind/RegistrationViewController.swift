f4aa34559128acdc2b011aaa2ab313d956f0368c
marwanalnounou <marwanalnounou@gmail.com>
2022-11-30 23:17:12 -0800

    Added registration file

---
+++ ReMind App/ReMind/ReMind/RegistrationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegistrationViewController.swift
+//  ReMind
+//
+//  Created by Marwan Alnounou on 11/30/22.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func noAccountClicked(_ sender: UIButton) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
